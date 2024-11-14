be021ae48290210a57e349b9b86092d86c5e6a94
SUYAB123 <111383970+SUYAB123@users.noreply.github.com>
2024-11-14 14:09:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+    if (str === '') {
+        return '';
+    }
+    const spaceIndex = str.indexOf(' ');
+    if (spaceIndex === -1) {
+        return str;
+    }
+    return str.slice(0, spaceIndex);
 }
-
-// Do not change the code below
-
-const s = prompt("Enter String:");
-alert(firstWord(s));
